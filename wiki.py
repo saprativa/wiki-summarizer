22c2bf447ded06e371e7574d32883b1ef0560ae0
irakr <tarak.rigia@gmail.com>
2016-02-21 21:28:43 +0530

    Added funtion: print_first_para

--- wiki.py
+++ wiki.py
@@ -1,12 +1,19 @@
+############################## Wikipedia summarizer ###########################
+#
+#
+
 import requests 
 from bs4 import BeautifulSoup
 
 #asking the user for search keyword
 keyword = raw_input('Search Wikipedia for: ')
+
 #prefix is the common url part
 prefix = 'https://en.wikipedia.org/wiki/'
+
 #making the complete url prefix + keyword
 url = prefix + keyword.strip().replace(' ', '_')
+
 #printing the url
 print 'URL: ' + url
 
@@ -33,6 +40,7 @@ def print_infobox_table(table):
 			tds = row.find_all('td')	
 			if len(ths) is not 0:
 				for i, th in enumerate(ths):
+					#For the Heading part inside the table
 					if (len(ths) is 1) and (len(tds) is 0):
 						print '::' + th.text.strip() + '::'
 						continue
@@ -48,5 +56,36 @@ def print_infobox_table(table):
 					else:
 						print td.text.strip().replace('\n', ', ') + ',',
 
+
+#Function to extract and display the first paragraph.
+def print_first_para(soup):
+	body = soup.body	#Portion of interest : 'body'
+	div1 = body.find(id='mw-content-text')
+	para = []	#A list to store each paragraph before contents table
+	stop_f = False	#Flag for stopping the traversing process
+	#Search and extract begins
+	for c in div1.contents:
+		if c.name is None:	#Directly skip
+			#print 'check 1'
+			continue
+		if c.name == 'div':
+			#print 'check 2'
+			if c.has_attr('id')==True:
+				#print 'check 3'
+				if c['id']=="toc":
+					#print 'check 4'
+					break
+		elif c.name == 'p':
+			#print 'check 5'
+			para.append(c.text)
+		else:
+			#print 'check 6'
+			continue	
+	del c
+	#print para[1]
+	for c in para:
+		print c,'*********************************************'
+
 #calling print_infobox_table
-print_infobox_table(soup.find('table', class_='infobox'))
+#print_infobox_table(soup.find('table', class_='infobox'))
+print_first_para(soup)
